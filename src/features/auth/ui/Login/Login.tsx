30dada2ab3ff61b58342e1bd339d1403fdc2f805
Artem_Teplygin <teplygin07@mail.ru>
2025-10-20 16:54:29 +0300

    chore: remove placeholder text from Login.tsx

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -58,18 +58,7 @@ export const Login = () => {
       >
         <FormControl>
           <FormLabel sx={{ "&.Mui-focused": { color: theme.palette.text.secondary } }}>
-            <p>
-              To login get registered
-              <a
-                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
-                href="https://social-network.samuraijs.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                here
-              </a>
-            </p>
-            <p>or use common test account credentials:</p>
+            <p>Use common test account credentials:</p>
             <p>
               <b>Email:</b> free@samuraijs.com
             </p>
